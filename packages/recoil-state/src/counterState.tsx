92cae5878bffecaf7c0748e9cbe3727c3f4c599c
haki-user <adityapratapsingh33@outlook.com>
2023-09-12 14:52:06 +0530

    little snackbar fix

--- packages/recoil-state/src/counterState.tsx
+++ packages/recoil-state/src/counterState.tsx
@@ -35,6 +35,6 @@ export const SnackbarState = atom<SnackbarStateType>({
     message: "",
     show: false,
     duration: 4000,
-    transform: "translateX(100%)",
+    transform: "translateX(110%)",
   },
 });
