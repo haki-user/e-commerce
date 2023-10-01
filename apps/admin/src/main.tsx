984a70b37aa29701631b81da2f9994a6fd347147
haki-user <adityapratapsingh33@outlook.com>
2023-10-01 22:24:46 +0530

    admin build fix

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="1014648950758-2764p31r7c9ebv9hqnfbruqi27dlnjfd.apps.googleusercontent.com">
         <React.StrictMode>
-            <App />
+            {/* <App /> */}
         </React.StrictMode>
     </GoogleOAuthProvider>,
 )
